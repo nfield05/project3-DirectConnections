8678aae850ed64cf5d102616af12c0d120c60caf
alyssah38 <148991442+alyssah38@users.noreply.github.com>
2024-11-27 12:52:40 -0500

    SRC File with simple UI set up

---
+++ Header.js
@@ -0,0 +1,10 @@
+// components/Header.js
+const Header = () => {
+    return (
+        <header>
+            <h1>NFL Direct Connections</h1>
+        </header>
+    );
+};
+
+export default Header;
