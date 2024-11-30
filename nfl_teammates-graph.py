5eb874ee37a7e1e92dee01297463b65b1808f9e3
nfield05 <124720739+nfield05@users.noreply.github.com>
2024-11-30 17:54:14 -0600

    Creates graph with nodes as a player in a given year with teammates as edges.

---
+++ nfl_teammates-graph.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import time
+from collections import deque
+
+
+class Graph:
+    # Graph structure to handle players and teammates
+    def __init__(self):
+        self.adjacency_list = {}
+
+    def add_edge(self, player1, player2, weight=1):
+        # Add an edge between two players who are teammates in a given year
+        if player1 not in self.adjacency_list:
+            self.adjacency_list[player1] = []
+        if player2 not in self.adjacency_list:
+            self.adjacency_list[player2] = []
+        self.adjacency_list[player1].append((player2, weight))
+        self.adjacency_list[player2].append((player1, weight))
